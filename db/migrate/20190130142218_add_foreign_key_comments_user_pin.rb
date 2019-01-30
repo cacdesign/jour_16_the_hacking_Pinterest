05c8d9323b16188894a6217906a8f7c5ba404201
cacdesign <pro.nicolasfernandez@gmail.com>
2019-01-30 15:55:19 +0100

    ok

---
+++ db/migrate/20190130142218_add_foreign_key_comments_user_pin.rb
@@ -0,0 +1,6 @@
+class AddForeignKeyCommentsUserPin < ActiveRecord::Migration[5.2]
+  def change
+  	add_reference :comments, :user, foreign_key: true
+  	add_reference :comments, :pin, foreign_key: true
+  end
+end
